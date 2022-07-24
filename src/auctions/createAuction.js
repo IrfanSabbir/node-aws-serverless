23d3992f67599df91c6672c28963d1299f531f45
Irfan <avoidirfan@gmail.com>
2022-07-25 00:30:45 +0600

    Dynamodb Table, put operation and IAM policy

--- src/auctions/createAuction.js
+++ src/auctions/createAuction.js
@@ -1,4 +1,7 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
 
+const documentClient = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
 
@@ -6,13 +9,22 @@ async function createAuction(event, context) {
 
   const currentDate =  new Date();
   const auction = {
+    __typename: 'AuctionsTable',
+    id: uuid(),
     title,
     status: 'OPEN',
     createdAt: currentDate.toISOString()
   }
+
+  const params = {
+    TableName : 'AuctionsTable',
+    Item: auction
+  };
+
+  await documentClient.put(params).promise();
   return {
     statusCode: 200,
-    body: JSON.stringify({ auction, event }),
+    body: JSON.stringify({ auction }),
   };
 }
 
